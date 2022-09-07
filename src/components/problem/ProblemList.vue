5d9a9c98985360f166e7cc097b045b875e26de42
Tanix <mynameisltx@mail.ustc.edu.cn>
2022-09-07 21:00:56 +0800

    add pagination to ProblemList

--- src/components/problem/ProblemList.vue
+++ src/components/problem/ProblemList.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from "vue";
+import { ref, reactive, onMounted, computed, watch } from "vue";
 import { useUrlStore } from "@/stores/url";
+import router from "@/router";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import { AgGridVue } from "ag-grid-vue3";
-import router from "@/router";
+import type {
+  GridReadyEvent,
+  GridApi,
+  RowClickedEvent,
+} from "ag-grid-community";
 
 const total_count = ref(0);
 const page_size = ref(0);
+const total_page = computed((): number => {
+  if (page_size.value === 0) return 0;
+  else return Math.ceil(total_count.value / page_size.value);
+});
+const page = ref(1);
+
+// problem类型定义
 type problem_type = {
   id: number;
   title: string;
@@ -44,24 +56,40 @@ const column_defs = reactive([
   },
 ]);
 
-function row_clicked(event: any) {
-  const problem: problem_type = event.data;
-  router.push({ name: "problem-detail", params: { id: problem.id } });
+// 获取grid的api
+let grid_api: GridApi | null = null;
+function grid_ready(event: GridReadyEvent) {
+  grid_api = event.api;
 }
 
+// 获取数据到table
 async function get_problems(offset: number) {
+  if (grid_api) grid_api.showLoadingOverlay();
   const problems_url = useUrlStore().problems_url;
   problems_url.searchParams.set("offset", offset.toString());
   const resp = await fetch(problems_url);
   const data = await resp.json();
-  return data;
-}
-
-onMounted(async () => {
-  const data = await get_problems(0);
   page_size.value = data.page_size;
   total_count.value = data.total_count;
   row_data.value = data.problems;
+  if (grid_api) grid_api.hideOverlay();
+}
+
+// 改变页数, 重新从服务器获取数据
+watch(page, async (new_page: number) => {
+  await get_problems((new_page - 1) * page_size.value);
+});
+
+// table行被点击进入problem详情页面
+function row_clicked(event: RowClickedEvent<problem_type>) {
+  const problem = event.data;
+  if (problem)
+    router.push({ name: "problem-detail", params: { id: problem.id } });
+}
+
+// 初始获取offset0的数据
+onMounted(async () => {
+  get_problems(0);
 });
 </script>
 
@@ -69,16 +97,22 @@ onMounted(async () => {
   <v-container>
     <v-card>
       <v-card-text>
+        <!-- problem列表 -->
         <ag-grid-vue
-          style="height: 75vh"
+          style="height: 74vh; margin-bottom: 0; padding-bottom: 0"
           align="center"
           class="ag-theme-material"
           :suppressCellFocus="true"
           :columnDefs="column_defs"
           :rowData="row_data"
           :paginationPageSize="page_size"
+          @grid-ready="grid_ready"
           @rowClicked="row_clicked"
         />
+        <!-- 分页 -->
+        <v-container style="padding: 0">
+          <v-pagination v-model="page" :length="total_page" />
+        </v-container>
       </v-card-text>
 
       <!-- v-data-table将在3.1版本发布, 可能更名为v-table -->
